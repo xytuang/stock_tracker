fc6415119c76d8ce0c91f15b1d49cc6364554b3f
Xiang Yu Tuang <xiangyutuang@gmail.com>
2024-07-23 19:39:18 -0400

    adjustments

--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -3,10 +3,12 @@ dotenv.config();
 
 const PORT: number = 8080;
 const API_KEY: string = process.env.API_KEY || '';
-const BASE_URL: string = 'https://yahoo-finance15.p.rapidapi.com/api'
+const PASS = process.env.PASS;
+const BASE_URL: string = 'https://yahoo-finance15.p.rapidapi.com/api';
 
 export {
     PORT,
+    PASS,
     API_KEY,
     BASE_URL
 }
